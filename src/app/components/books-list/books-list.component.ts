b7bce93b379ad25f48297b2dd4b411ca645337ab
Yury Kuzmich <yurakuzmich@gmail.com>
2021-02-27 23:21:26 +0300

    feat: add click event in books list

--- src/app/components/books-list/books-list.component.ts
+++ src/app/components/books-list/books-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IBook } from 'src/app/models/book';
 import { BooksService } from 'src/app/services/books.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'ng-books-list',
@@ -10,12 +11,18 @@ import { BooksService } from 'src/app/services/books.service';
 export class BooksListComponent implements OnInit {
 
   booksList :IBook[] | undefined;
+  cartService;
 
-  constructor(books: BooksService) {
+  constructor(books: BooksService, cartService: CartService) {
     this.booksList = books.getBooks();
+    this.cartService = cartService;
   }
 
   ngOnInit(): void {
+  }
+
+  onBuy(book: IBook) {
+    this.cartService.addBook(book);
   }
 
 }
